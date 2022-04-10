d8055ce0fca37732e46eeeb10ed2929606db5bef
john-mirage <chaosdynamix@posteo.net>
2022-04-10 16:52:35 +0200

    feat: add desktop rules for dashboard

--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,4 @@ import "@styles/components/header.css";
 import "@styles/components/toggle-button.css";
 import "@styles/components/dashboard.css";
 import "@styles/components/social-media.css";
+import "@styles/components/stat.css";
