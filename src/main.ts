9c4a6842dc3817e17f6b72481386ad480bb6b92d
john-mirage <chaosdynamix@posteo.net>
2022-04-10 16:12:16 +0200

    feat: add header and top dashboard for mobile

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import "@styles/reset.css";
 import "@styles/variables.css";
 import "@styles/globals.css";
+
+import "@styles/components/page.css";
 import "@styles/components/header.css";
+import "@styles/components/toggle-button.css";
+import "@styles/components/dashboard.css";
+import "@styles/components/social-media.css";
